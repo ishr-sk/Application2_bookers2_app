3ad0c97c00bdc3cadfb3f0114d60d44e921deb6f
ishr-sk <kmrttnr1208@gmail.com>
2021-06-16 15:23:49 +0000

    [追加]:book編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
   
   devise_for :users
-    resources :books, only: [:new, :create, :index, :show, :destroy] do
+    resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
   end
   resources :users, only: [:show, :edit, :update]
 end
