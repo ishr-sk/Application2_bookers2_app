79e7f83653e92a929b712d3f63f067d335b0b3b4
ishr-sk <kmrttnr1208@gmail.com>
2021-06-16 14:44:44 +0000

    [更新]:ユーザ画面から投稿可能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bfc10a61b525ed05afea81bcf34a1b4b80b683f0018a33f54d7c480f6860f3181ffbd595152071bc1d7c7032ae742ae2148ac7dff537191e40ccf1d8a5cf9f32'
